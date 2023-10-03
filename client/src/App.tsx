7dc6bfa30c25aad89a1fd72155d09effe06b2d1a
Viraaj SIngh Gill <viraajgill2@gmail.com>
2023-10-04 00:40:06 +0530

    1st push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { themeSettings } from "./theme";
 import { Box, CssBaseline } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
@@ -15,7 +16,7 @@ function App() {
             <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
               <Navbar></Navbar>
               <Routes>
-                <Route path="/" element={<div>dashboard page</div>} />
+                <Route path="/" element={<Dashboard/>} />
                 <Route
                   path="/predictions"
                   element={<div>predictions page</div>}
